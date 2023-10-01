113cc6d33ededa9d5b016eab4e766199200c0aad
ferney <ferney.montoyag@gmail.com>
2023-10-01 10:13:16 -0500

    Calculator on its mobile version finished v1

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,45 +1,108 @@
 import { buttons } from "../../utils/buttons-info";
 import "./Button.css";
 
-const btnTypes = {
-  number: "number",
-  symbol: "symbol",
-  deletion: "deletion",
-  reset: "reset",
-  equals: "equals",
+let termsCounter = 1;
+let termDotsRestriction = {
+  numero1: "",
 };
 
-const buttonNumberClicked = (key, setValueInput) => {
-  const btnValue = buttons[key].value;
+const addNumberToTerm = (numberToAdd) => {
+  const objIndex = termsCounter - 1;
+  const keyToEdit = Object.keys(termDotsRestriction)[objIndex];
+
+  termDotsRestriction[keyToEdit] += numberToAdd.toString();
+};
+
+const createAnotherTerm = () => {
+  termsCounter++;
+
+  termDotsRestriction[`numero${termsCounter}`] = "";
+};
+
+const buttonNumberClicked = (index, valueInput, setValueInput) => {
+  if (valueInput === "Math Error" || valueInput === "Infinity") {
+    buttonResetClicked(setValueInput);
+  }
+
+  const btnValue = buttons[index].value;
+  addNumberToTerm(btnValue);
 
   setValueInput((prev) => prev + btnValue.toString());
 };
 
-const buttonSymbolClicked = (key, setValueInput) => {
-  const btnValue = buttons[key].value;
+const buttonSymbolClicked = (index, valueInput, setValueInput) => {
+  if (valueInput === "Math Error" || valueInput === "Infinity") {
+    buttonResetClicked(setValueInput);
+  }
+
+  const btnValue = buttons[index].value;
   const lastChar = valueInput.charAt(valueInput.length - 1);
 
   const symbols = ["+", "-", ".", "/", "x", "="];
   const symbolValidation = symbols.includes(lastChar);
 
-  if (!symbolValidation) {
-    setValueInput((prev) => prev + btnValue);
+  if (
+    !symbolValidation &&
+    valueInput !== "" &&
+    valueInput !== "Math Error" &&
+    valueInput !== "Infinity"
+  ) {
+    if (btnValue !== ".") {
+      createAnotherTerm();
+    }
+    const currentIndexTerm = Object.keys(termDotsRestriction)[termsCounter - 1];
+    const isThereADot = termDotsRestriction[currentIndexTerm].includes(".");
+
+    if (isThereADot) {
+      if (!btnValue === ".") {
+        addNumberToTerm(btnValue);
+        setValueInput((prev) => prev + btnValue);
+      }
+    } else if (!isThereADot) {
+      addNumberToTerm(btnValue);
+      setValueInput((prev) => prev + btnValue);
+    }
   }
 };
 
-const buttonEqualsClicked = (setValueInput) => {
-  const mathOperation = eval(valueInput);
+const buttonEqualsClicked = (valueInput, setValueInput) => {
+  if (valueInput === "Math Error" || valueInput === "Infinity") {
+    buttonResetClicked(setValueInput);
+  }
 
-  buttonResetClicked();
+  try {
+    if (valueInput !== "") {
+      let mathOperation = eval(valueInput);
 
-  setValueInput(mathOperation.toString());
+      if (mathOperation % 1 !== 0) {
+        mathOperation = mathOperation.toFixed(5);
+      }
+
+      buttonResetClicked(setValueInput);
+
+      setValueInput(mathOperation);
+    }
+  } catch (error) {
+    setValueInput("Math Error");
+  }
 };
 
 const buttonResetClicked = (setValueInput) => {
+  const resetObj = {
+    numero1: "",
+  };
+  termDotsRestriction = resetObj;
+
+  termsCounter = 1;
+
   setValueInput("");
 };
 
-const buttonDeletionClicked = (setValueInput) => {
+const buttonDeletionClicked = (valueInput, setValueInput) => {
+  if (valueInput === "Math Error" || valueInput === "Infinity") {
+    buttonResetClicked(setValueInput);
+  }
+
   setValueInput((prev) => prev.slice(0, -1));
 };
 
@@ -59,7 +122,19 @@ function Button({
                 ${type}
                 ${theme}
             `}
-      onClick={() => {}}
+      onClick={() => {
+        if (type === "number") {
+          return buttonNumberClicked(index, valueInput, setValueInput);
+        } else if (type === "symbol") {
+          buttonSymbolClicked(index, valueInput, setValueInput);
+        } else if (type === "equals") {
+          buttonEqualsClicked(valueInput, setValueInput);
+        } else if (type === "deletion") {
+          buttonDeletionClicked(valueInput, setValueInput);
+        } else if (type === "reset") {
+          buttonResetClicked(setValueInput);
+        }
+      }}
     >
       {value}
     </button>
