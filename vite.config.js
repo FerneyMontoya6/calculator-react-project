59ce6ea58fac5fd6fed1dedde5cea1408329a0f8
ferney <ferney.montoyag@gmail.com>
2023-10-03 17:14:40 -0500

    docs:v1.0.0

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default {
-  base: '/calculator-react-project/',
-}
+export default defineConfig({
+  plugins: [react()],
+  base: 'https://ferneymontoya6.github.io/calculator-react-project/',
+})
